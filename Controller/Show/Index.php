a43a990467bb08aa73fdbef488513e00b7be3704
Jessica Garcia <jesgarsan@gmail.com>
2022-05-04 16:56:12 +0100

    Initial commit

---
+++ Controller/Show/Index.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Jessi\FrontendController\Controller\Show;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index implements HttpGetActionInterface
+{
+    /**
+     * @var PageFactory
+     */
+    private PageFactory $resultPageFactory;
+
+    /**
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * @return Page
+     */
+    public function execute(): Page
+    {
+        /** @var Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->set('Frontend Controller');
+        return $resultPage;
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Jessi FrontendController
+
+This module is created to explain how to create 
+a frontend controller.
